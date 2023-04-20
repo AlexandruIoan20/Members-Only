fbb2d22993416d70ddb4c3fac21da013ba0d49ad
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-04-20 09:39:05 +0300

    Profile controller - no tests

---
+++ members/controllers/profile_controller.js
@@ -0,0 +1,19 @@
+exports.profile_detail = (req, res, next) => { 
+    res.send(`NOT IMPLEMENTED: profile detail: ${req.params.id}`);
+};
+
+exports.delete_profile_get = (req, res, next) => { 
+    res.send("NOT IMPLEMENTED: delete profile GET");
+};
+
+exports.delete_profile_post = (req, res, next) => { 
+    res.send("NOT IMPLEMENTED: delete profile POST");
+};
+
+exports.update_profile_get = (req, res, next) => { 
+    res.send("NOT IMPLEMENTED: update profile GET");
+};
+
+exports.update_profile_post = (req, res, next) => { 
+    res.send("NOT IMPLEMENTED: update profile POST");
+};
