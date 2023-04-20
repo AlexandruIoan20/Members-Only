b07a02ff4059e9d8cb3df3bd3558d66ef98e1c53
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-04-20 09:04:54 +0300

    Models for database

---
+++ members/models/Member.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+const Schema = mongoose.Schema; 
+
+const MemberSchema = new Schema({ 
+    username: { type: String, minLength: 1, required: true }, 
+    password: { type: String, minLength: 1, required: true }, 
+    email: { type: String, minLength: 1, required: true }, 
+    birthDay: { type: String, minLength: 1, required: true }, 
+}); 
+
+MemberSchema.virtual("formatted_birthDay").get(function () { 
+    return DateTime.fromJSDate(this.birthDay).toLocaleString(DateTime.DATE_MED);
+});
+
+MemberSchema.virtual("url").get(function () { 
+    return; 
+}); 
+
+module.exports = mongoose.model("Member", MemberSchema);
