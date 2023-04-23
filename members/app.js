5c1ce35d261426d75a08abf874a528f08098a06a
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-04-23 18:57:22 +0300

    Delete user page

--- members/app.js
+++ members/app.js
@@ -33,7 +33,7 @@ const User = require("./models/User");
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -46,12 +46,12 @@ app.use(session(
     saveUninitialized: true,
   }
 ));
-app.use(passport.initialize());
+app.use(passport.initialize())
 app.use(passport.session()); 
 app.use(flash()); 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter)
 app.use("/general", generalRouter);
 
 // catch 404 and forward to error handler
--- members/controllers/profile_controller.js
+++ members/controllers/profile_controller.js
@@ -18,26 +18,38 @@ exports.profile_detail = async (req, res, next) => {
         }, 
 
         (err, results) => { 
+            let personalAccount = false; 
+            if(req.user.username == results.user.username) personalAccount = true; 
             if(err) return next(err);
 
             res.render("user_detail", { 
                 user: results.user, 
                 posts: results.posts, 
+                personalAccount,
             })
         }
     )
 };
 
 exports.delete_profile_get = (req, res, next) => { 
-    res.send("NOT IMPLEMENTED: delete profile GET");
+    res.render("user_delete", { 
+        user: req.user
+    });
 };
 
-exports.delete_profile_post = (req, res, next) => { 
-    res.send("NOT IMPLEMENTED: delete profile POST");
+exports.delete_profile_post = async (req, res, next) => { 
+    try { 
+        await User.deleteOne({ _id: req.params.id });
+        console.log(`User ${req.params.id} deleted`);
+
+        res.redirect("/general/register");
+    } catch (err) { 
+        if(err) return next(err);
+    }
 };
 
 exports.update_profile_get = (req, res, next) => { 
-    res.send("NOT IMPLEMENTED: update profile GET");
+
 };
 
 exports.update_profile_post = (req, res, next) => { 
