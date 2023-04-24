9fc271c45eb34ceb0c8c996b7bc7ea3ce601ab37
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-04-24 21:25:45 +0300

    Delete finished (create check) + start update

--- members/controllers/post _controller.js
+++ members/controllers/post _controller.js
@@ -3,6 +3,7 @@ const { body, validationResult } = require("express-validator");
 
 const Post = require("../models/Post");
 const Genre = require("../models/Genre");
+const User = require("../models/User");
 
 exports.post_detail = async (req, res, next) => {
     const post = await Post.findById(req.params.postId)
@@ -16,6 +17,7 @@ exports.post_detail = async (req, res, next) => {
     } else { 
         res.render("post_detail", { 
             post: post, 
+            reqUser: req.user, 
         })
     }
 };
@@ -70,16 +72,75 @@ exports.create_post_post = [
     }
 ]
 
-exports.delete_post_get = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: delete post GET");
+exports.delete_post_get = async (req, res, next) => {
+    async.parallel( 
+        { 
+            async user () { 
+                const u = await User.findById(req.params.id).exec(); 
+                return u; 
+            }, 
+
+            async post () { 
+                const p = await Post.findById(req.params.postId).exec(); 
+                return p; 
+            }
+        }, 
+        (err, results) => { 
+            if(err) return next(err); 
+
+            if(results.user === null) { 
+                const err = new Error("User not found"); 
+                err.status = 404; 
+                throw new Error(err); 
+            }
+
+            if(results.post === null) { 
+                const err = new Error("Post not found"); 
+                err.status = 404; 
+                throw new Error(err); 
+            }; 
+
+            res.render("post_delete", { 
+                post: results.post, 
+            })
+        }
+    )
 };
 
-exports.delete_post_post = (req, res, next) => { 
-    res.send("NOT IMPLEMENTED: delete post POST");
+exports.delete_post_post = async (req, res, next) => { 
+    await Post.findByIdAndDelete(req.params.postId);
+    res.redirect(`/general/profiles/${req.params.id}`);
 };
 
-exports.update_post_get = (req, res, next) => { 
-    res.send("NOT IMPLEMENTED: update post GET");
+exports.update_post_get = async (req, res, next) => { 
+    async.parallel( 
+        { 
+            async genres () { 
+                const g = await Genre.find( {} ).exec (); 
+                return g; 
+            }, 
+            async post () { 
+                const p = await Post.findById(req.params.postId); 
+                return p; 
+            }
+        }, 
+        (err, results) => { 
+            console.log(results); 
+            if(results.post === null) { 
+                const err = new Error ("Post not found"); 
+                err.status = 404; 
+                throw new Error(err);
+            };
+
+            res.render("post_form", { 
+                pageTitle: `Edit post: ${results.post.title}`, 
+                title: results.post.title, 
+                description: results.post.description,  
+                genres: results.genres, 
+            })
+
+        }
+    )
 };
 
 exports.update_post_post = (req, res, next) => { 
