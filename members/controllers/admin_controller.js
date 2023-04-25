60364db23cedcdd7b3505de0904b79a68979c862
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-04-25 18:48:49 +0300

    Promote system

--- members/controllers/admin_controller.js
+++ members/controllers/admin_controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const { GRADES } = require("../global/global_user"); 
 
+const { body, validationResult } = require("express-validator");
+
 exports.promote_get = async (req, res, next) => { 
     try { 
         const user = await User.findById(req.params.id).exec(); 
@@ -13,6 +15,34 @@ exports.promote_get = async (req, res, next) => {
     }
 };
 
-exports.promote_post = async (req, res, next) => { 
-    res.send("Promote page"); 
-}
+exports.promote_post = [ 
+    body("grade", "grade is required!")
+        .trim() 
+        .escape(), 
+
+    async (req, res, next) => { 
+        const errors = validationResult(req);
+        const lastUser = await User.findById(req.params.id); 
+
+        if(!errors.isEmpty() ){
+            res.render("user_promote", { 
+                user: lastUser, 
+                GRADES, 
+                errors: errors.array(), 
+            }); 
+        }
+
+        const user = new User({ 
+            username: lastUser.username, 
+            email: lastUser.email, 
+            password: lastUser.password, 
+            grade: req.body.grade, 
+            birthDay: lastUser.birthDay, 
+            _id: lastUser._id, 
+        }); 
+
+        await User.findByIdAndUpdate(lastUser._id, user); 
+        console.log(`User ${user.username} promoted.`); 
+        res.redirect(user.url);
+    }
+]
